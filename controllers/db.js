593f2d62f84a15c2e73bd56500672cd43d576d94
Alexander Reyes <alexandereliasr9@gmail.com>
2020-04-04 23:37:55 -0400

    Check hsot

--- controllers/db.js
+++ controllers/db.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize');
-
+console.log(process.env.DB_HOST);
 // Option 1: Passing parameters separately
 const sequelize = new Sequelize(process.env.DB_DATABASE,process.env.DB_USER , process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
